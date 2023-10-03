d9e4dfde593b36dab04b8c2b94f2e8d80e5d7327
Valery Noskov <rahdah06@gmail.com>
2023-10-03 15:58:24 +0300

    Create simpleRadioChecker.js
    init

---
+++ simpleRadioChecker.js
@@ -0,0 +1,14 @@
+function checkRadios(value) {
+    Array.from(document.getElementsByClassName("check_input"))
+        .forEach(function (item) {
+            if (item.value == value)
+                item.checked = true
+        })
+}
+
+checkRadios(3) //оценка, которую надо поставить:
+//1 - полностью не согласен
+//2 - скорее не согласен
+//3 - отчасти согласен, отчасти нет (default)
+//4 - скорее согласен
+//5 - полностью согласен
